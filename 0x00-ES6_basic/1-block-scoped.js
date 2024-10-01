f30070fa047f98624a57beb49b8427f870effba8
mmanyuat <mmanyuat201@gmail.com>
2024-10-01 05:29:07 +0000

    next

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,6 @@
-export default function taskBlock(trueOrFalse) {
+export default function taskBlock() {
   const task = false;
   const task2 = true;
-
-  if (trueOrFalse) {
-	  const innertask = true;
-	  const innertask2 = false;
-  }
 
   return [task, task2];
 }
